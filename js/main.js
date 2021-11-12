614ea3e3e9cc58fb63993c72df26a546a9f9df6f
Kostya <rodina9180gaocheng@ukr.net>
2021-11-12 17:56:09 +0200

    completed adaptivity

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
-$(function(){
+$(function () {
    $('.reviews-slider').slick({
       infinite: true,
       slidesToShow: 2,
       slidesToScroll: 2,
       arrows: false,
-      dots: true
+      dots: true,
+      responsive: [
+         {
+           breakpoint: 1106,
+           settings: {
+             slidesToShow: 1,
+             slidesToScroll: 1,
+           }
+         },
+      ]
    });
- });
+
+   $('.btn__menu').on('click', function(){
+      $('.menu__list').slideToggle();
+   });
+});
